f13d824aa2b79f36e87d748eaf84e484411aacb5
joshbam-joshwilly <j.alana@alustudent.com>
2024-02-19 17:17:51 +0100

    readme

---
+++ P_1/Main.java
@@ -0,0 +1,10 @@
+import java.util.StringJoiner;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
+
+
+public class Main {
+    public static void main(String[] args) {
+        
+    }
+}
